637383d9238f5c832d454a67556fb01580486fdb
Evan <md.mofazzalhossain18@gmail.com>
2024-12-22 22:43:28 +0600

    PizzaSHopEnum

--- Chapter-3/PracticeCode/3.4.4. Enumerated Types/src/PizzaOrder/Pizza.java
+++ Chapter-3/PracticeCode/3.4.4. Enumerated Types/src/PizzaOrder/Pizza.java
@@ -19,4 +19,19 @@ public class Pizza {
     }
 
 }
+/***
+What It Does
+Constructor:
 
+A constructor is a special method in Java that is called when an object is created using the new keyword.
+The constructor's purpose is to initialize the fields of the class with the provided values.
+Parameters:
+
+The constructor takes a parameter Size size,
+ which represents the size of the pizza (SMALL, MEDIUM, LARGE, or EXTRA_LARGE).
+        this.size:
+
+The this keyword refers to the current object.
+        this.size is the size field of the Pizza class.
+By writing this.size = size;, you are assigning the value of the size parameter to the size field of the object.
+***/
