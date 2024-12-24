232cc9333abb2c1551ff7ea6504b272ad6d34462
Evan <md.mofazzalhossain18@gmail.com>
2024-12-24 21:37:05 +0600

    MathMethods

---
+++ Chapter-3/PracticeCode/3.5. Operators/src/Calculation/PowerCalculation.java
@@ -0,0 +1,11 @@
+package Calculation;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        double x = 22.2;
+        double t = 324.23;
+        double re = Math.pow(t,x);
+        double sq = Math.sqrt(x);
+        System.out.println(re +" "+ sq);
+    }
+}
