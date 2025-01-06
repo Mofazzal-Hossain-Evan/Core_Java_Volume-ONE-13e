a96f3f6520e41bb7d35ac0ae3a375e2d59f4b237
Evan <md.mofazzalhossain18@gmail.com>
2025-01-07 03:13:08 +0600

    DataValidationTool

--- Chapter-3/PracticeCode/3.8.6. Statements That Break Control Flow/src/DataValidationTool.java
+++ Chapter-3/PracticeCode/3.8.6. Statements That Break Control Flow/src/DataValidationTool.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class DataValidationTool {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.println("Enter integers to calculate their sum (negative numbers will be ignored):");
+        System.out.println("Enter 0 to finish.");
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            int num = input.nextInt();
+
+            if(num ==  0) {
+                break;
+            }
+
+            if(num == 0) {
+                break;
+            }
+
+            if(num < 0) {
+                System.out.println("Negative number ignored.");
+                continue;
+            }
+            sum = sum + num;
+        }
+        System.out.println("The sum of valid numbers is: " + sum);
+        input.close();
+    }
 }
