3e95ad776ebdd11a3b99b88d1b01dd7db22aa95a
Mofazzal-Hossain-Evan <md.mofazzalhossain18@gmail.com>
2025-05-26 22:16:26 +0600

    RESTART IDE

---
+++ src/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    protected double result;
+
+    public void displayResult(){
+        System.out.println("Result: "+ result);
+    }
+
+    public void executeOperation(String operationName, Object...args){
+
+        try{
+
+        }
+    }
+}
