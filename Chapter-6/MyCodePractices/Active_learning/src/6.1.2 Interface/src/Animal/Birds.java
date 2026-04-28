0b75af7b8463cecf0f0b3b5e2d71505efa70e31b
Evan <119051240+Mofazzal-Hossain-Evan@users.noreply.github.com>
2026-04-28 21:43:58 +0600

    6.1.2

--- Chapter-6/MyCodePractices/Active_learning/src/6.1.2 Interface/src/Animal/Birds.java
+++ Chapter-6/MyCodePractices/Active_learning/src/6.1.2 Interface/src/Animal/Birds.java
@@ -2,11 +2,26 @@ package Animal;
 
 public class Birds implements Flyable{
 
+    String name;
+    int age;
+    String color;
+    boolean sound;
+    boolean isflayable;
+    public Birds(String name, int age, String color, boolean sound, boolean isflayable){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.sound = sound;
+        this.isflayable = isflayable;
+    }
 
+Flyable fly;
 
 
     @Override
     public boolean Flyable(boolean bird) {
         return false;
     }
+
+
 }
